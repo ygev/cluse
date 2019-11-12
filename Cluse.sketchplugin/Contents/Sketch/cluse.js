cdd53cc2fcab708df0b95d8b3ca25d1c9cd45a5d
Yana <yanacommit@wizwar.net>
2019-11-11 20:00:58 -0500

    Fetch initial color from layers into interface.

--- Cluse.sketchplugin/Contents/Sketch/cluse.js
+++ Cluse.sketchplugin/Contents/Sketch/cluse.js
@@ -1,27 +1,34 @@
-var sketch = require('sketch/dom')
-var async = require('sketch/async')
-var DataSupplier = require('sketch/data-supplier')
-var UI = require('sketch/ui')
-var Settings = require('sketch/settings')
+var sketch = require('sketch/dom');
+var async = require('sketch/async');
+var DataSupplier = require('sketch/data-supplier');
+var UI = require('sketch/ui');
+var Settings = require('sketch/settings');
 
 function onApply(options) {
-        console.log("onApply(options)")
+        console.log("onApply(options)");
         UI.alert("Test Alert", "goose!");
 };
 
 function onLoad(webView) {
         console.error("onLoad()");
         var doc = sketch.getSelectedDocument();
-        var selection = doc.selectedLayers;
+        var selection = doc.selectedLayers;       
+        var bgSketch = selection.layers[0].style.fills[0].color;
+        var fgSketch = selection.layers[1].style.textColor;
         console.log("selected #: " + selection.length);
-        console.log("layer 0: " + selection.layers[0].style.fills[0].color);
-        console.log("layer 1: " + selection.layers[1].style.fills[0].color);
+        console.log("layer 0: " + bgSketch);
+        console.log("layer 1: " + fgSketch);
 
-        webView.evaluateJavaScript_completionHandler("setColorInit('#fff', '#000')", null);
+        console.log("calling " + `setColorInit('${bgSketch}', '${fgSketch}')`);
+        // webView.evaluateJavaScript_completionHandler("setColorInit('#fff', '#000')", null);
+
+        webView.evaluateJavaScript_completionHandler(`setColorInit('${bgSketch}', '${fgSketch}')`, null);
 }
+
+
 
 // TODO for Oct 29
 // Assuming that two layers are selected, figure out which one is textColor, and which one is just a Fill. 
 // Extract both and store in f(text) and b(fill) respectively.
 
-module.exports = { onApply, onLoad};
+module.exports = { onApply, onLoad };    
