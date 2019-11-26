bedc7902f9f754859a875d1a33748b0c850e2a85
Yana <yanacommit@wizwar.net>
2019-11-26 13:39:14 -0500

    Clear up comments for findTxtSize()

--- Cluse.sketchplugin/Contents/Sketch/cluse.js
+++ Cluse.sketchplugin/Contents/Sketch/cluse.js
@@ -44,7 +44,7 @@ function findTxtSize(){
         var txtSize = selection.layers[1].style.fontSize;
         var txtWeight = selection.layers[1].style.fontWeight;
 
-        // if weight is above this, then bold=true, else bold=false.
+        // If font weight is above Sketch level 5, then bold=true, else bold=false.
         var bold;
         if (txtWeight > 5) {
                 bold = true;
@@ -53,10 +53,7 @@ function findTxtSize(){
              bold = false;
         }
 
-        // shorter alternative to the above:
-        //var bold = txtWeight > 5;
-
-        // if tit size is above 18 AND bold ORRRR above 24 and NOT bold, it is Large Text, else normal text
+        // If font size is above 18px AND bold or above 24px and NOT bold, it is Large Text, else normal text.
         if ((txtSize >= 18 && bold == true) || txtSize >= 24) {
                return true;
         }
